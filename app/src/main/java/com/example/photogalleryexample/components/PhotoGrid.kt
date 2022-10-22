c765df931769863a18d353108f97ab76fc769d08
Ethan <edenvir99@gmail.com>
2022-10-22 17:12:56 +0900

    fix: project structure

--- app/src/main/java/com/example/photogalleryexample/components/PhotoGrid.kt
+++ app/src/main/java/com/example/photogalleryexample/components/PhotoGrid.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.example.photogalleryexample.R
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
